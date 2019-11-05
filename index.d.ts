0089f7329e728659c5a679b4fd5074aa9d675e94
Doma <leishenghao@126.com>
2019-11-05 18:56:15 +0800

    Add TypeScript definitions (#4)
    * Add TypeScript definitions
    
    * Add definition for ResponsiveNav.Item

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+import Nav, { NavProps } from 'rsuite/es/Nav/Nav';
+import { DropdownMenuProps } from 'rsuite/es/Dropdown/DropdownMenu';
+
+interface Props<T> extends NavProps<T> {
+  /**
+   * More drop-down menu props
+   */
+  moreProps?: DropdownMenuProps;
+
+  /**
+   * More text displayed on the drop-down
+   */
+  moreText?: React.ReactNode;
+
+  /**
+   * The callback function when removing options
+   */
+  onItemRemove?(eventKey: T): void;
+
+  /**
+   * Options can be removed
+   */
+  removable?: boolean;
+}
+
+class ResponsiveNav<T> extends React.Component<Props<T>> {
+  static Item: typeof Nav['Item'];
+}
+
+export default ResponsiveNav;
